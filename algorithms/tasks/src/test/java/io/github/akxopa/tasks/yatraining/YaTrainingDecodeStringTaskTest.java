1a2f48e02422aee3bb373a819333f4fea2886a47
apoxka <apoxka@yandex.com>
2026-06-11 20:21:42 +0300

    fix(workflow): path for pom

--- algorithms/tasks/src/test/java/io/github/akxopa/tasks/yatraining/YaTrainingDecodeStringTaskTest.java
+++ algorithms/tasks/src/test/java/io/github/akxopa/tasks/yatraining/YaTrainingDecodeStringTaskTest.java
@@ -3,7 +3,6 @@ package io.github.akxopa.tasks.yatraining;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import io.github.akxopa.tasks.yatraining.YaTrainingDecodeStringTask;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
